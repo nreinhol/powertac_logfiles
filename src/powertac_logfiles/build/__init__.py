10e9edcc46075a9b0de94897fb2596a6cb7fd6ab
Peter Kings <p.kings@icloud.com>
2018-12-05 17:08:23 +0100

    add more logfiles for generation

--- src/powertac_logfiles/build/__init__.py
+++ src/powertac_logfiles/build/__init__.py
@@ -7,14 +7,21 @@ from .utils import get_log_files  # noqa
 from .make import make_log_files  # noqa
 from .make import make_web_log_files  # noqa
 
-
 # Constants for local processing
 
 LOG_FILES = {'BrokerImbalanceCost': 'org.powertac.logtool.example.BrokerImbalanceCost',
              'BrokerCosts': 'org.powertac.logtool.example.BrokerCosts',
              'TariffMktShare': 'org.powertac.logtool.example.TariffMktShare',
              'BrokerAccounting': 'org.powertac.logtool.example.BrokerAccounting',
+             'MktPriceStats': 'org.powertac.logtool.example.MktPriceStats',
+             'TotalDemand': 'org.powertac.logtool.example.TotalDemand',
+             'CustomerStats': 'org.powertac.logtool.example.CustomerStats',
+             'BrokerBalancingActions': 'org.powertac.logtool.example.BrokerBalancingActions'}
+LOG_FILES = {'BrokerImbalanceCost': 'org.powertac.logtool.example.BrokerImbalanceCost',
+             'BrokerCosts': 'org.powertac.logtool.example.BrokerCosts',
+             'TariffMktShare': 'org.powertac.logtool.example.TariffMktShare',
+             'BrokerAccounting': 'org.powertac.logtool.example.BrokerAccounting',
              'MktPriceStats': 'org.powertac.logtool.example.MktPriceStats'}
-LOG_FILES = {'BrokerAccounting': 'org.powertac.logtool.example.BrokerAccounting'}
+# LOG_FILES = {'BrokerBalancingActions': 'org.powertac.logtool.example.BrokerBalancingActions'}
 # GAME_NUMBERS = list(np.random.choice(300, 5, replace=False) + 1)
 GAME_NUMBERS = list(range(1, 325))
