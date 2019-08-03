f430d50a3aa0003cbc3e4bc2d58b548d7e9d45c3
Peter Kings <p.kings@icloud.com>
2019-08-03 15:56:20 +0200

    improve prediction performance plot and add accuracy

--- src/powertac_logfiles/visualize/prediction_performance.py
+++ src/powertac_logfiles/visualize/prediction_performance.py
@@ -9,7 +9,8 @@ import ewiis3DatabaseConnector as data
 
 def evaluate_prediction(game_id, df_prediction, type):
     df_prediction = calculate_prediction_performance(df_prediction)
-    plot_prediction_performance(game_id, df_prediction, type, show_outliers=False)
+    df_accuracy =  calculate_accuracy(df_prediction)
+    plot_prediction_performance(game_id, df_prediction, df_accuracy, type, show_outliers=False)
     # plot_prediction_performance(game_id, df_prediction, show_outliers=True)
 
 
@@ -24,31 +25,40 @@ def calculate_prediction_performance(df_prediction):
     return df_prediction
 
 
-def plot_prediction_performance(game_id, df_prediction, type, show_outliers=False):
+def plot_prediction_performance(game_id, df_prediction, df_accuracy, type, show_outliers=False):
+
     sns.set(font_scale=visualize.FIGURE_FONT_SCALE)
     sns.set_style(style=visualize.FIGURE_STYLE)
     fig = plt.figure(figsize=visualize.FIGSIZE_LANDSCAPE)
     # fig.suptitle("Imbalance", fontsize=16)
-    ax2 = fig.add_subplot(511)
-    ax2.set_title("Mean Absolut Error")
+    ax2 = fig.add_subplot(411)
+    ax2.set_title("Absolut Error")
     ax2 = sns.boxplot(ax=ax2, x="Proximity", y="mae", data=df_prediction[['Proximity', 'mae']],
                       showfliers=show_outliers)
-    ax3 = fig.add_subplot(512)
-    ax3.set_title("Mean Squared Error")
+    ax2.set_ylabel("Value")
+
+    ax3 = fig.add_subplot(412)
+    ax3.set_title("Squared Error")
     ax3 = sns.boxplot(ax=ax3, x="Proximity", y="mse", data=df_prediction[['Proximity', 'mse']],
                       showfliers=show_outliers)
-    ax4 = fig.add_subplot(513)
+    ax3.set_ylabel("Value")
+
+    """ax4 = fig.add_subplot(413)
     ax4.set_title("Root Mean Squared Error")
     ax4 = sns.boxplot(ax=ax4, x="Proximity", y="rmse", data=df_prediction[['Proximity', 'rmse']],
-                      showfliers=show_outliers)
-    ax5 = fig.add_subplot(514)
-    ax5.set_title("Mean Absolut Percentage Error")
+                      showfliers=show_outliers)"""
+
+    ax5 = fig.add_subplot(413)
+    ax5.set_title("Absolut Percentage Error")
     ax5 = sns.boxplot(ax=ax5, x="Proximity", y="mape", data=df_prediction[['Proximity', 'mape']],
                       showfliers=show_outliers)
-    ax6 = fig.add_subplot(515)
-    ax6.set_title("Correct Sign")
-    ax6 = sns.countplot(ax=ax6, x="Proximity", hue='Correct sign',
-                        data=df_prediction[['Proximity', 'Correct sign']])
+    ax5.set_ylabel("Value")
+
+    ax6 = fig.add_subplot(414)
+    ax6.set_title("Accuracy of Sign")
+    ax6 = sns.lineplot(ax=ax6, x="Proximity", y="accuracy", data=df_accuracy)
+    ax6.set_ylabel("Value")
+
     fig.tight_layout()
 
     outliers = "_show_outliers" if show_outliers else ""
@@ -63,11 +73,10 @@ def correctly_classified(row):
 
 
 def calculate_accuracy(df_prediction):
-    df_total_counts = df_prediction.groupby(['Proximity']).size().reset_index(name='counts')
     df_accuracy = df_prediction.groupby(['Proximity', 'Correct sign']).size().reset_index(name='counts')
-    df_accuracy = df_accuracy.apply(lambda x: calculate_accuracy_for_row(x, df_total_counts), axis=1)
-    print(df_accuracy)
-
-def calculate_accuracy_for_row(row, df_total_counts):
-    total_count = df_total_counts[df_total_counts["Proximity"]==row['Proximity']]['counts'][0]
-    print(total_count)
+    # print(df_accuracy)
+    df_number_of_total_predictions = df_prediction.groupby(['Proximity']).size().reset_index(name='counts')
+    # print(df_number_of_total_predictions)
+    df_accuracy = df_accuracy[df_accuracy['Correct sign']].reset_index()
+    df_accuracy['accuracy'] = df_accuracy['counts'] / df_number_of_total_predictions['counts']
+    return df_accuracy
